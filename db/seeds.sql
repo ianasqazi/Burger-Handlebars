4788659f3058b41a347a0c7af318582b95012534
Anas Qazi <ianasqazi@gmail.com>
2019-12-15 12:13:45 -0500

    db schema and seeds to create database structure completed

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,22 @@
--- insert burgers here
+/*
+
+To run this file, we do the following in our Terminal:
+
+1. Go to the directory of this sql file.
+
+2. Get into our mysql console.
+
+3. Run "source seed.sql"
+
+*/
+
+-- Insert into burgers a set of records.
+INSERT INTO burgers (burger_name) VALUES ('The Krusty Burger');
+INSERT INTO burgers (burger_name) VALUES ('The Ghost Burger');
+INSERT INTO burgers (burger_name) VALUES ('The Varsity Burger');
+INSERT INTO burgers (burger_name) VALUES ('The White Castle Slider');
+INSERT INTO burgers (burger_name) VALUES ('The Rays Hell Burger');
+INSERT INTO burgers (burger_name) VALUES ('The Ramen Burger');
+INSERT INTO burgers (burger_name) VALUES ('The Jucy Lucy');
+INSERT INTO burgers (burger_name) VALUES ('The Lab-Grown Burger');
+INSERT INTO burgers (burger_name) VALUES ('The Quadruple Bypass Burger');
