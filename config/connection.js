9c2eea1b45047efbf283f7af7deafbb70a31e4b0
Anas Qazi <ianasqazi@gmail.com>
2019-12-22 13:44:18 -0500

    updated connection to add jawsdb

--- config/connection.js
+++ config/connection.js
@@ -5,15 +5,24 @@ var connection;
 
 if(process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL)
-} else {
-  connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "",
-    database: "burgers_db"
-  });
-};
+} 
+// else {
+//   connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     password: "",
+//     database: "burgers_db"
+//   });
+  else {
+    connection = mysql.createConnection({
+      host: "qzkp8ry756433yd4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+      port: 3306,
+      user: "rtek3u6oarqbixih",
+      password: "kwyrfcv18ka21uom",
+      database: "dnkmz6iqgg8quphe"
+    });
+  };
 
 // Make connection.
 // connection.connect(function(err) {
