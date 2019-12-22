c1295c374821dee0f791df9fe311eb525fe4de45
Anas Qazi <ianasqazi@gmail.com>
2019-12-22 13:49:50 -0500

    updated connection to add jawsdb

--- config/connection.js
+++ config/connection.js
@@ -6,14 +6,6 @@ var connection;
 if(process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL)
 } 
-// else {
-//   connection = mysql.createConnection({
-//     host: "localhost",
-//     port: 3306,
-//     user: "root",
-//     password: "",
-//     database: "burgers_db"
-//   });
   else {
     connection = mysql.createConnection({
       host: "qzkp8ry756433yd4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
